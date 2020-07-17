c6faa0c204eedbdd96fca527ead2b69593c4e167
ericmax-gomez <68430235+ericmax-gomez@users.noreply.github.com>
2020-07-17 10:30:09 -0300

    Update main.c
    Ahi estpy probando cambiar algo para q se entienda mejor.. Aca hacemos lo q pide la entrega sin adelantarnos..

--- main.c
+++ main.c
@@ -13,20 +13,21 @@ int main()
     //nombres y apellidos ---- GRUPO 12 ----
 
     srand(time(0));
-
-    char nombre[10], apellido [10];
+    
+    Int tamanio=10;
+    char nombre[tamanio], apellido [tamanio];
 
     int dni, cant, carton1[COLUMNA][RENGLON],carton2[COLUMNA][RENGLON],carton3[COLUMNA][RENGLON];
-    int ccarton1[COLUMNA][RENGLON],ccarton2[COLUMNA][RENGLON],ccarton3[COLUMNA][RENGLON];
+    int maquina1[COLUMNA][RENGLON],maquina2[COLUMNA][RENGLON],maquina3[COLUMNA][RENGLON];
 
 
     cargarjugadores(nombre,apellido,dni);
 
     cant=cantidadCartones();
 
-    llenarCarton (carton1,carton2,carton3,ccarton1,ccarton2,ccarton3,cant);
+    llenarCarton (carton1,carton2,carton3,maquina1,maquina2,maquina3,cant);
 
-    mostrarCartones(carton1,carton2,carton3,ccarton1,ccarton2,ccarton3,cant);
+    mostrarCartones(carton1,carton2,carton3,maquina1,maquina2,maquina3,cant);
 
     return 0;
 
