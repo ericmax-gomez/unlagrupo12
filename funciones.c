53f588505a32396bceef566b4881ecf5f8fed7f2
ericmax-gomez <68430235+ericmax-gomez@users.noreply.github.com>
2020-08-22 14:00:25 -0300

    Update funciones.c

--- funciones.c
+++ funciones.c
@@ -2,18 +2,17 @@
 #include <stdlib.h>
 #include <time.h>
 #include "funciones.h"
+#include "procedimientos.h"
 
+int  busquedaSecuencial(int m[][RENGLON], int buscar, int col){
 
+    int pos = - 1 ;
 
-int busquedaSecuencial(int m[COLUMNA][RENGLON], int buscar){
+    for( int j = 0 ; j <col; j ++) {
 
-    int pos = -1;
+         for( int i = 0 ; i <RENGLON; i ++) {
 
-    for(int j=0;j<COLUMNA;j++){
-
-        for ( int i = 0; i<RENGLON; i++){
-
-            if ( m[j][i]==buscar){ //Lo encontre
+            if (m [j] [i] == buscar) { // Lo encontre
 
                 pos = i;
 
@@ -21,176 +20,62 @@ int busquedaSecuencial(int m[COLUMNA][RENGLON], int buscar){
         }
     }
 
-       //Bien menos
-       // if ( pos == -1){ printf("No lo encontre");}
+       // Bien menos
+       // if (pos == -1) {printf ("No lo encontre");}
 
     return pos;
 }
-
-
-void cargarjugadores(char nombre[],char apellido[],int dni){
-
-    printf("ingrese nombre... \n");
-    scanf("%s",nombre);
-    printf("ingrese apellido... \n");
-    scanf("%s",apellido);
-    printf("ingrese dni... \n");
-    scanf("%d",&dni);
-}
-
-
-
-int cantidadCartones (){
-    int cant=0;
+int  cantidadCartones(){
+    int cant = 0 ;
     do{
-        printf("eliga con cuantos cartones va a jugar...\n --> 1\n --> 2\n --> 3\n ");
+        printf ( " eliga con cuantos cartones va a jugar ... \n -> 1 \n -> 2 \n -> 3 \n  " );
         scanf("%d",&cant);
 
 
-    }while (cant<1 || cant>3);
+    } while (cant < 1 || cant> 3 );
+
     return cant;
 }
-
-int tipoDeCarton(){
+int  tipoDeCarton(){
 
     int op;
 
-    printf("elegir opcion: \n 1) comprar un carton cualquiera (aleatorio) \n 2) comprar carton personalizado (eligiendpo los numeros)\n");
-    scanf("%d",&op);
+    printf(" elegir opción:\n" );
+    printf("1) comprar un cartón cualquiera (aleatorio)\n");
+    printf("2) comprar un cartón personalizado (eligiendpo los numeros)\n");
+    scanf ( "%d",&op);
 
     return op;
 
 }
+int menuPrincipal(){
 
-void rellenarAleatorio (int carto[COLUMNA][RENGLON]){
+    int op=0;
 
-    int resultadoBusqueda=0;
-    int nuevoNumero=0;
+    printf("\n elige la opcion deseada: \n");
+    printf(" 1 - mostrar cartones \n");
+    printf(" 2 - mostrar bolillas sorteadas \n");
+    printf(" 3 - mostrar puntaje \n");
+    printf(" 4 - sacar proxima bolilla \n");
+    printf(" 5 - abandonar juego \n");
+    scanf("%d",&op);
 
-    for ( int i =0; i<COLUMNA; i++){
+    return op;
+}
+int  busquedaSecuencialVector( int m[90], int buscar, int contador){
+    int pos = - 1 ;
 
-        for ( int j= 0; j<RENGLON; j++ ){
+    for( int j = 0 ; j <contador; j ++) {
 
-            do{
-                nuevoNumero = 1 +rand()%91;
-                resultadoBusqueda = busquedaSecuencial(carto, nuevoNumero); //nos da la posicion ---> -1
+            if (m[j] == buscar) { // Lo encontre
 
-            }while(resultadoBusqueda!=-1);
-
-            carto[i][j]= nuevoNumero;
+                pos = j;
 
         }
     }
-}
 
+       // Bien menos
+       // if (pos == -1) {printf ("No lo encontre");}
 
-void rellenarPersonalizado (int carto[COLUMNA][RENGLON]){
-
-    int resultadoBusqueda=0;
-    int nuevoNumero=0;
-
-    for ( int i =0; i<COLUMNA; i++){
-
-        for ( int j= 0; j<RENGLON; j++ ){
-
-            do{
-                do{
-                    printf("ingrese numero: \n");
-                    scanf("%d",&nuevoNumero);
-                }while(nuevoNumero<1 || nuevoNumero>90);
-
-                resultadoBusqueda = busquedaSecuencial(carto, nuevoNumero); //nos da la posicion ---> -1
-
-                if(resultadoBusqueda!=-1){
-                    printf("no es posible repetir numero, ");
-                }
-
-            }while(resultadoBusqueda!=-1);
-
-            carto[i][j]= nuevoNumero;
-
-        }
-    }
-}
-
-
-
-void  mostrarCarton(int cart[COLUMNA][RENGLON]){
-
-    for ( int i =0; i<RENGLON; i++){
-
-        for ( int j= 0; j<COLUMNA; j++ ){
-
-               printf("%d    ", cart[j][i]) ;
-        }
-
-        printf("\n");
-    }
-    printf("\n\n\n");
-
-}
-
-void  mostrarCartones(int cart1[COLUMNA][RENGLON],int cart2[COLUMNA][RENGLON],int cart3[COLUMNA][RENGLON],int ccar1[COLUMNA][RENGLON],int ccar2[COLUMNA][RENGLON],int ccar3[COLUMNA][RENGLON],int c){
-
-    //muestro el primer carton
-
-    mostrarCarton(cart1);
-    mostrarCarton(ccar1);
-
-    if(c>1){ //muestro el segundo si existe
-
-        mostrarCarton(cart2);
-        mostrarCarton(ccar2);
-
-    }
-
-    if(c>2){//muestro el tercero si existe
-
-        mostrarCarton(cart3);
-        mostrarCarton(ccar3);
-
-    }
-
-}
-
-
-void llenarTipo (int car[COLUMNA][RENGLON]){
-
-        int op=tipoDeCarton();
-
-        if(op==1){
-
-            rellenarAleatorio(car);
-        }
-
-        else{
-
-            rellenarPersonalizado(car);
-       }
-
-}
-
-
-
-void llenarCarton (int car1[COLUMNA][RENGLON],int car2[COLUMNA][RENGLON],int car3[COLUMNA][RENGLON],int ccar1[COLUMNA][RENGLON],int ccar2[COLUMNA][RENGLON],int ccar3[COLUMNA][RENGLON],int c){
-
-    //lleno el primer carton
-
-    llenarTipo(car1);
-    rellenarAleatorio(ccar1);
-
-    if(c>1){ //lleno el segundo si existe
-
-        llenarTipo(car2);
-        rellenarAleatorio(ccar2);
-
-    }
-
-    if(c>2){//lleno el tercero si existe
-
-        llenarTipo(car3);
-        rellenarAleatorio(ccar3);
-
-    }
-
+    return pos;
 }
