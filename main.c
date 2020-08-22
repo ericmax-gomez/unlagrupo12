e0e96163dc73fc761b257b859928e80b0f5f61e4
ericmax-gomez <68430235+ericmax-gomez@users.noreply.github.com>
2020-08-22 13:57:16 -0300

    Update main.c

--- main.c
+++ main.c
@@ -2,32 +2,59 @@
 #include <stdlib.h>
 #include <time.h>
 #include "funciones.h"
-//#include "funcionesMatrices.h"
-
-
+#include "procedimientos.h"
 
 int main()
-
 {
+     srand ( time(0));
 
-    //nombres y apellidos ---- GRUPO 12 ----
+     int COLUMNA=5;
+     int opcion=0;
+     int cant=0;
+     int bolilla[90];
+     int contador=0;
+     int carton1 [COLUMNA][RENGLON];
+     int carton2 [COLUMNA][RENGLON];
+     int carton3 [COLUMNA][RENGLON];
+     int maquina1 [COLUMNA][RENGLON];
+     int maquina2 [COLUMNA][RENGLON];
+     int maquina3 [COLUMNA][RENGLON];
 
-    srand(time(0));
+        cant = cantidadCartones();
 
-    char nombre[10], apellido [10];
+        switch(cant){
+            case 1:llenarCarton(carton1, maquina1, COLUMNA); break;
+            case 2:llenarCarton(carton1, maquina1, COLUMNA);
+                   llenarCarton(carton2, maquina2, COLUMNA); break;
+            case 3:llenarCarton(carton1, maquina1, COLUMNA);
+                   llenarCarton(carton2, maquina2, COLUMNA);
+                   llenarCarton(carton3, maquina3, COLUMNA);break;
+        }
 
-    int dni, cant, carton1[COLUMNA][RENGLON],carton2[COLUMNA][RENGLON],carton3[COLUMNA][RENGLON];
-    int ccarton1[COLUMNA][RENGLON],ccarton2[COLUMNA][RENGLON],ccarton3[COLUMNA][RENGLON];
+        do{
+                system("pause");
+                system("cls");
+            opcion=menuPrincipal();
+            switch(opcion){
+                case 1:switch(cant){
+                        case 1:mostrarCartones(carton1, maquina1, COLUMNA);break;
+                        case 2:mostrarCartones(carton1, maquina1, COLUMNA);
+                               mostrarCartones(carton2, maquina2, COLUMNA);break;
+                        case 3:mostrarCartones(carton1, maquina1, COLUMNA);
+                               mostrarCartones(carton2, maquina2, COLUMNA);
+                               mostrarCartones(carton3, maquina3, COLUMNA);break;
+                        };break;
+                case 2:mostrarBolillasSorteadas(bolilla, contador);break;
+                case 3:break;
+                case 4:bolillas(bolilla, contador);
+                       contador= contador+1;break;
+                case 5:printf("\n GRACIAS POR JUGAR \n");break;
+                default:break;
 
-
-    cargarjugadores(nombre,apellido,dni);
-
-    cant=cantidadCartones();
-
-    llenarCarton (carton1,carton2,carton3,ccarton1,ccarton2,ccarton3,cant);
-
-    mostrarCartones(carton1,carton2,carton3,ccarton1,ccarton2,ccarton3,cant);
+            }
+        }while(opcion!=5);
 
     return 0;
-
 }
+
+
